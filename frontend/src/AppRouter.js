5ae8abf4cc778fdaea4032184be29a769206e448
billc27 <110593711+billc27@users.noreply.github.com>
2023-07-23 00:10:28 +0700

    fix: fix router

--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Test from "./pages/test2"; // Import your page component here
 import App from "./App"; // Import your default App component here
 
 function AppRouter() {
@@ -8,11 +7,9 @@ function AppRouter() {
       <Router>
         <Routes>
             <Route path="/" element={<App />} />
-            <Route path="/test2" element={<Test />} />
         </Routes>
       </Router>
     );
 }
 
 export default AppRouter;
-  
