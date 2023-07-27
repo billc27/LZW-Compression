67f5b9f5b8514ae40f6b24c47040d8260a911725
billc27 <110593711+billc27@users.noreply.github.com>
2023-07-27 21:42:47 +0700

    feat: added design for history details

--- frontend/src/components/history-details.js
+++ frontend/src/components/history-details.js
@@ -7,13 +7,16 @@ export default function HistoryDetails(props) {
         : selectedHistoryItem.text;
 
     return (
-        <div>
-            <h1>History Details</h1>
-            <p>Input: {inputText}</p>
-            <p>Mode: {selectedHistoryItem.mode}</p>
-            <p>Algorithm: {selectedHistoryItem.algorithm}</p>
-            <p>Additional Info: {selectedHistoryItem.additional}</p>
-            <p>Result: {selectedHistoryItem.result.replace(/,/g, " ")}</p>
+        <div className="bg-web-color h-full font-sans font-semibold p-5 text-white">
+            <h1 className="text-center text-2xl">History Details</h1>
+            <div className="text-lg">
+                <p>Input: {inputText}</p>
+                <p>Mode: {selectedHistoryItem.mode}</p>
+                <p>Algorithm: {selectedHistoryItem.algorithm}</p>
+                <p>Additional Info: {selectedHistoryItem.additional}</p>
+                <p>Result: {selectedHistoryItem.result.replace(/,/g, " ")}</p>
+            </div>
+            
         </div>
     );
 }
